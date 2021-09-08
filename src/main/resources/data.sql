c10611be4e750f73384fc4b97989571026b622ab
Emerson Prado <emersonpradoo@hotmail.com>
2021-09-08 00:17:17 -0300

    Implementando Autenticação e Autorização com Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$ZCQUfvyQMwc2.HTZxSMeMeAc6tdpU.e9xdbtvUY8T2MiMzTBwk7fG');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
