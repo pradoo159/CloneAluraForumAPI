1df1636b6ddd75a3fa40daf715c03aed1f41480c
Emerson Prado <emersonpradoo@hotmail.com>
2021-09-07 23:19:15 -0300

    Implementando paginação e ordenação ao listar tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -30,70 +34,71 @@ import br.com.alura.forum.repository.TopicoRepository;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
-	
-	
+
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
-	
 
-	// Ao passar uma string no como parâmetro do método, é possível enviar esse parâmetro via query params
+	// Ao passar uma string no como parâmetro do método, é possível enviar esse
+	// parâmetro via query params
 	@GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
-    	if(nomeCurso  == null) {
-    		List<Topico> topicos = topicoRepository.findAll();
-    		return TopicoDto.converter(topicos);
-    	} else {
-    		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
-    		return TopicoDto.converter(topicos);
-    	}
-    }
-    
-    @PostMapping
-    @Transactional
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
-    	Topico topico = form.converter(cursoRepository);
-    	topicoRepository.save(topico);
-    	
-    	URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
-    	return ResponseEntity.created(uri).body(new TopicoDto(topico));
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
-    	
-    	Optional<Topico> topico = topicoRepository.findById(id);
-    	if(topico.isPresent()) {
-    		return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
-    	}
-    	
-    	return ResponseEntity.notFound().build();
-    }
-    
-    @PutMapping("/{id}")
-    @Transactional
-    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
-    	Optional<Topico> optional = topicoRepository.findById(id);
-    	if(optional.isPresent()) {
-    		Topico topico = form.atualizar(id, topicoRepository);
-    		return ResponseEntity.ok(new TopicoDto(topico));
-    	}
-    	
-    	return ResponseEntity.notFound().build();
-    }
-    
-    @DeleteMapping("/{id}")
-    @Transactional
-    public ResponseEntity<?> remover(@PathVariable Long id) {
-    	Optional<Topico> topico = topicoRepository.findById(id);
-    	if(topico.isPresent()) {
-    		topicoRepository.deleteById(id);
-    		return ResponseEntity.noContent().build();
-    	}
-    	
-    	return ResponseEntity.notFound().build();
-    }
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+
+		if (nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDto.converter(topicos);
+		} else {
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDto.converter(topicos);
+		}
+	}
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
+
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
 
 }
